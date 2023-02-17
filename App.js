c1bc5996e22bd2c2e739599ffd7b5946eda4613b
Vadim Savin <savinvadim1312@gmail.com>
2023-02-17 17:53:28 +0000

    3d animation

--- App.js
+++ App.js
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// @ts-nocheck
+import { Canvas, useFrame, useLoader } from '@react-three/fiber/native';
+import { useState, useRef, Suspense, useLayoutEffect } from 'react';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import { TextureLoader } from 'expo-three';
+import { useAnimatedSensor, SensorType } from 'react-native-reanimated';
 
-export default function App() {
+function Box(props) {
+  const [active, setActive] = useState(false);
+
+  const mesh = useRef();
+
+  useFrame((state, delta) => {
+    if (active) {
+      mesh.current.rotation.y += delta;
+      mesh.current.rotation.x += delta;
+    }
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+    >
+      <boxGeometry />
+      <meshStandardMaterial color={active ? 'green' : 'gray'} />
+    </mesh>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Shoe(props) {
+  const [base, normal, rough] = useLoader(TextureLoader, [
+    require('./assets/Airmax/textures/BaseColor.jpg'),
+    require('./assets/Airmax/textures/Normal.jpg'),
+    require('./assets/Airmax/textures/Roughness.png'),
+  ]);
+
+  const material = useLoader(MTLLoader, require('./assets/Airmax/shoe.mtl'));
+
+  const obj = useLoader(
+    OBJLoader,
+    require('./assets/Airmax/shoe.obj'),
+    (loader) => {
+      material.preload();
+      loader.setMaterials(material);
+    }
+  );
+
+  const mesh = useRef();
+
+  useLayoutEffect(() => {
+    obj.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.material.map = base;
+        child.material.normalMap = normal;
+        child.material.roughnessMap = rough;
+      }
+    });
+  }, [obj]);
+
+  useFrame((state, delta) => {
+    let { x, y, z } = props.animatedSensor.sensor.value;
+    x = ~~(x * 100) / 5000;
+    y = ~~(y * 100) / 5000;
+    mesh.current.rotation.x += x;
+    mesh.current.rotation.y += y;
+  });
+
+  return (
+    <mesh ref={mesh} rotation={[0.7, 0, 0]}>
+      <primitive object={obj} scale={10} />
+    </mesh>
+  );
+}
+
+export default function App() {
+  const animatedSensor = useAnimatedSensor(SensorType.GYROSCOPE, {
+    interval: 100,
+  });
+
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+
+      <Suspense fallback={null}>
+        <Shoe animatedSensor={animatedSensor} />
+      </Suspense>
+
+      {/* <Box />
+      <Box position={[0, 2, 0]} />
+      <Box position={[0, -2, 0]} /> */}
+    </Canvas>
+  );
+}
---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// metro.config.js
+module.exports = {
+  resolver: {
+    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs'],
+    assetExts: ['glb', 'gltf', 'mtl', 'obj', 'png', 'jpg'],
+  },
+};
